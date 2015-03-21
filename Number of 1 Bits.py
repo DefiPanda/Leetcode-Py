2daa9b6109473a58cbfc04134e10639bd1e0b5b2
resorcap <resorcap@users.noreply.github.com>
2015-03-21 14:27:40 +0800

    Update Number of 1 Bits.py

--- Number of 1 Bits.py
+++ Number of 1 Bits.py
@@ -2,6 +2,6 @@ class Solution:
     def hammingWeight(self, n):
         count = 0
         while n:
-            count += 1;
-            n = (n-1) & n
+            count += 1
+            n = (n - 1) & n
         return count
